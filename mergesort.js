43d3585dff412055097eb9ccc811e85bc4c5940f
Beno Suriano <bjsuriano@gmail.com>
2019-04-02 09:33:51 -0600

    Refactored split.

--- mergesort.js
+++ mergesort.js
@@ -1,63 +1,64 @@
-function split(wholeArray) {
-  /* your code here to define the firstHalf and secondHalf arrays */
-  if (!Array.isArray(wholeArray)) {
-    return [wholeArray]
-  }
-  let firstHalf, secondHalf;
 
-  firstHalf = wholeArray.slice(0, Math.round(wholeArray.length/2));
-  secondHalf = wholeArray.slice(Math.round(wholeArray.length/2), wholeArray.length);
+// function merge(arr1, arr2) {
 
-  return [firstHalf, secondHalf];
+//   // initialize a merged array that's going to hold the result
+//   let mergedArr = []
+//   // repeat until there are no values left in the arr's
+//   while (arr1.length > 0 || arr2.length > 0) {
+//     // take the first element of each array
+//     let element1 = arr1[0]
+//     let element2 = arr2[0]
+
+//     if (element1 == undefined) {
+//       mergedArr.push(arr2.shift())
+
+//     } else if (element2 == undefined) {
+//       mergedArr.push(arr1.shift())
+//     }
+//     // check which is lower
+//     if (element1 < element2) {
+//       //push lower to merged array
+//       mergedArr.push(arr1.shift())
+//     } else if(element2 < element1) {
+//       mergedArr.push(arr2.shift())
+//     }
+//   }
+//   return mergedArr
+// }
+
+// // merge([1],[2]);
+
+
+// function mergeSort(arr) {
+//   let splitArr
+//   let firstHalf, secondHalf
+//   let merged
+
+//   if (arr.length === 1) {
+//     return arr
+//   } else {
+//     splitArr = split(arr)
+
+//     firstHalf = mergeSort(splitArr[0])
+//     secondHalf = mergeSort(splitArr[1])
+
+//     merged = merge(firstHalf, secondHalf)
+//     // console.log(merged);
+//     return merged
+//   }
+// }
+
+// mergeSort([2,1])
+
+function split(array) {
+  let firstHalf = []
+  let secondHalf = []
+  let length = array.length
+
+  firstHalf = array.slice(0, Math.ceil(length/2))
+  secondHalf = array.slice(Math.ceil(length/2))
+
+  return [firstHalf, secondHalf]
 }
 
-function merge(arr1, arr2) {
 
-  // initialize a merged array that's going to hold the result
-  let mergedArr = [];
-  // repeat until there are no values left in the arr's
-  while (arr1.length > 0 || arr2.length > 0) {
-    // take the first element of each array
-    let element1 = arr1[0];
-    let element2 = arr2[0];
-
-    if (element1 == undefined) {
-      mergedArr.push(arr2.shift());
-
-    } else if (element2 == undefined) {
-      mergedArr.push(arr1.shift());
-    }
-    // check which is lower
-    if (element1 < element2) {
-      //push lower to merged array
-      mergedArr.push(arr1.shift());
-    } else if(element2 < element1) {
-      mergedArr.push(arr2.shift());
-    }
-  }
-  return mergedArr;
-}
-
-// merge([1],[2]);
-
-
-function mergeSort(arr) {
-  let splitArr;
-  let firstHalf, secondHalf;
-  let merged;
-
-  if (arr.length === 1) {
-    return arr;
-  } else {
-    splitArr = split(arr);
-
-    firstHalf = mergeSort(splitArr[0]);
-    secondHalf = mergeSort(splitArr[1]);
-
-    merged = merge(firstHalf, secondHalf);
-    // console.log(merged);
-    return merged;
-  }
-}
-
-mergeSort([2,1]);
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,30 +1,30 @@
-describe("Split Array function", function() {
+describe('Split Array function', function() {
 
 
-  it("is able to split an array into two halves", function() {
+  it('is able to split an array into two halves', function() {
     // your code here
-    expect(split([1,2,3,4])).toEqual([[1,2],[3,4]]);
-    expect(split([1,2,3,4,5])).toEqual([[1,2,3],[4,5]]);
-  });
-});
+    expect(split([1,2,3,4])).toEqual([[1,2],[3,4]])
+    expect(split([1,2,3,4,5])).toEqual([[1,2,3],[4,5]])
+  })
+})
 
-describe("Merge function", function() {
-  it("is able to merge two sorted arrays into one sorted array", function() {
+describe('Merge function', function() {
+  it('is able to merge two sorted arrays into one sorted array', function() {
     // test the merging algorithm
     // expect( merge([1]) ).toEqual([1]);
-    expect( merge([2],[1]) ).toEqual([1,2]);
-    expect( merge([1,2],[3,4]) ).toEqual([1,2,3,4]);
-    expect( merge([1,3],[2,4]) ).toEqual([1,2,3,4]);
-    expect( merge([1,4],[2,3]) ).toEqual([1,2,3,4]);
-    expect( merge([1,2,4],[3,5]) ).toEqual([1,2,3,4,5]);
-  });
-});
+    expect( merge([2],[1]) ).toEqual([1,2])
+    expect( merge([1,2],[3,4]) ).toEqual([1,2,3,4])
+    expect( merge([1,3],[2,4]) ).toEqual([1,2,3,4])
+    expect( merge([1,4],[2,3]) ).toEqual([1,2,3,4])
+    expect( merge([1,2,4],[3,5]) ).toEqual([1,2,3,4,5])
+  })
+})
 
-describe("Merge sort function", function() {
+describe('Merge sort function', function() {
   it('split an unsorted array and merge it together sorted', function(){
     //expect( mergeSort ([1]) ).toEqual([1]);
-    expect( mergeSort ([2,1]) ).toEqual([1,2]);
-    expect( mergeSort ([3,1,2]) ).toEqual([1,2,3]);
-    expect( mergeSort ([3,4,1,5,7])).toEqual([1,3,4,5,7]);
-  });
-});
+    expect( mergeSort ([2,1]) ).toEqual([1,2])
+    expect( mergeSort ([3,1,2]) ).toEqual([1,2,3])
+    expect( mergeSort ([3,4,1,5,7])).toEqual([1,3,4,5,7])
+  })
+})
